2d14e62e86636dce660c991f6546f4aa832bb2f5
hina81 <i20203@e.hiroshima-cu.ac.jp>
2025-01-22 20:00:46 +0900

    管理者ログイン画面の作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    get 'dashboard/index'
+  end
   devise_for :admins
   root 'homes#top'
   resources :reservations, only: [:new, :create, :index, :edit, :update, :show, :destroy ]
