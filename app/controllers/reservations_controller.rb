fba671b1aab2c0aa818561d59bfb61a23a87c6ab
hina81 <i20203@e.hiroshima-cu.ac.jp>
2025-01-22 15:37:59 +0900

    routingの記述変更

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -8,6 +8,10 @@ class ReservationsController < ApplicationController
   def index
   end
 
+  def show
+    
+  end
+
   def edit
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,4 @@
 Rails.application.routes.draw do
-  get 'reservations/new'
-  get 'reservations/create'
-  get 'reservations/index'
-  get 'reservations/edit'
-  get 'reservations/update'
-  get 'reservations/destroy'
   root 'homes#top'
   resources :reservations, only: [:new, :create, :index, :edit, :update, :show, :destroy ]
   devise_for :users
