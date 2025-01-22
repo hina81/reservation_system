3cffa38de5ab658075f60106844199d75ce5afe0
hina81 <i20203@e.hiroshima-cu.ac.jp>
2025-01-22 19:51:28 +0900

    adminモデルの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :admins
   root 'homes#top'
   resources :reservations, only: [:new, :create, :index, :edit, :update, :show, :destroy ]
   devise_for :users
