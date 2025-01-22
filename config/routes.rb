f9268bd309fd7623e86f03a62513d2da3339d3b8
hina81 <i20203@e.hiroshima-cu.ac.jp>
2025-01-22 12:59:58 +0900

    controllerの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'reservations/new'
+  get 'reservations/create'
+  get 'reservations/index'
+  get 'reservations/edit'
+  get 'reservations/update'
+  get 'reservations/destroy'
+  get 'homes/top'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
