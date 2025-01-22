e58c2da7cecf7df633bced32f458e291619e1d2c
hina81 <i20203@e.hiroshima-cu.ac.jp>
2025-01-22 12:21:52 +0900

    deviseの導入

--- db/migrate/20250122022316_create_reservations.rb
+++ db/migrate/20250122022316_create_reservations.rb
@@ -1,7 +1,8 @@
 class CreateReservations < ActiveRecord::Migration[7.0]
   def change
     create_table :reservations do |t|
-      
+      $ rails g devise User
+
       t.string :name
       t.string :email
       t.string :phone
